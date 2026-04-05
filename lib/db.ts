510912edefcacc813700dc4905a286f6b204da87
ohdove <ashleytwy4@gmail.com>
2026-04-06 01:17:19 +0800

    Amend db.ts to read from .env.local instead of hardcoding credentials

--- lib/db.ts
+++ lib/db.ts
@@ -5,5 +5,5 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  port: Number(process.env.DB_PORT) || 5432,
 });
