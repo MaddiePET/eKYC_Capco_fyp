8dd4dc9b889c5c743ff14dcabdc35d0d972e3e93
MaddiePET <myatpaneithu.nge@gmail.com>
2026-06-21 21:37:37 +0800

    changed demo vid

--- app/page.tsx
+++ app/page.tsx
@@ -239,7 +239,7 @@ export default function Home() {
                   muted
                   preload="auto"
                 >
-                  <source src="/video/demo.mov" type="video/mp4" />
+                  <source src="/video/demo.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
               </div>
